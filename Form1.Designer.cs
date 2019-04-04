0ce117fcb0b79343db5a813c7773098dd859e952
Merve <47145223+mervegulozcan@users.noreply.github.com>
2019-04-04 17:24:19 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ArrayList_ve_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lstData = new System.Windows.Forms.ListBox();
+            this.btnDizi = new System.Windows.Forms.Button();
+            this.btnArrayList = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnList2 = new System.Windows.Forms.Button();
+            this.btnRenklendir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(402, 87);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(236, 252);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // lstData
+            // 
+            this.lstData.FormattingEnabled = true;
+            this.lstData.Location = new System.Drawing.Point(28, 87);
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(343, 251);
+            this.lstData.TabIndex = 1;
+            // 
+            // btnDizi
+            // 
+            this.btnDizi.Location = new System.Drawing.Point(28, 33);
+            this.btnDizi.Name = "btnDizi";
+            this.btnDizi.Size = new System.Drawing.Size(75, 23);
+            this.btnDizi.TabIndex = 2;
+            this.btnDizi.Text = "Array";
+            this.btnDizi.UseVisualStyleBackColor = true;
+            this.btnDizi.Click += new System.EventHandler(this.btnDizi_Click);
+            // 
+            // btnArrayList
+            // 
+            this.btnArrayList.Location = new System.Drawing.Point(147, 33);
+            this.btnArrayList.Name = "btnArrayList";
+            this.btnArrayList.Size = new System.Drawing.Size(75, 23);
+            this.btnArrayList.TabIndex = 3;
+            this.btnArrayList.Text = "Array List";
+            this.btnArrayList.UseVisualStyleBackColor = true;
+            this.btnArrayList.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(270, 33);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 4;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnList2
+            // 
+            this.btnList2.Location = new System.Drawing.Point(402, 33);
+            this.btnList2.Name = "btnList2";
+            this.btnList2.Size = new System.Drawing.Size(75, 23);
+            this.btnList2.TabIndex = 5;
+            this.btnList2.Text = "Butonlar";
+            this.btnList2.UseVisualStyleBackColor = true;
+            this.btnList2.Click += new System.EventHandler(this.btnList2_Click);
+            // 
+            // btnRenklendir
+            // 
+            this.btnRenklendir.Location = new System.Drawing.Point(540, 33);
+            this.btnRenklendir.Name = "btnRenklendir";
+            this.btnRenklendir.Size = new System.Drawing.Size(75, 23);
+            this.btnRenklendir.TabIndex = 6;
+            this.btnRenklendir.Text = "Renklendir";
+            this.btnRenklendir.UseVisualStyleBackColor = true;
+            this.btnRenklendir.Visible = false;
+            this.btnRenklendir.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.btnRenklendir);
+            this.Controls.Add(this.btnList2);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.btnArrayList);
+            this.Controls.Add(this.btnDizi);
+            this.Controls.Add(this.lstData);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ListBox lstData;
+        private System.Windows.Forms.Button btnDizi;
+        private System.Windows.Forms.Button btnArrayList;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnList2;
+        private System.Windows.Forms.Button btnRenklendir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace ArrayList_ve_List
+{
+    public partial class Form1 : Form
+    {   Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDizi_Click(object sender, EventArgs e)
+        {
+            lstData.Items.Clear();
+            int[] dizi = new int[5];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = rnd.Next(1, 25);
+                lstData.Items.Add(dizi[i]);
+            }
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lstData.Items.Clear();
+            ArrayList list = new ArrayList();
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    list.Add(rnd.Next(1, 25));
+            //}
+            list.Add(1);
+            list.Add(1.05);
+            list.Add("Ali");
+            list.Add(200);
+            list.Add("Fil");
+
+            foreach (var item in list)
+            {
+                lstData.Items.Add(item);
+            }
+
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lstData.Items.Clear();
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(rnd.Next(1, 25));
+            }
+            foreach (int item in list)
+            {
+                lstData.Items.Add(item);
+            }
+            
+
+        }
+
+        private List<Button> dugmeler = new List<Button>();
+        private void btnList2_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            dugmeler.Clear();
+            for (int i = 0; i < 7; i++)
+            {
+                Button btn = new Button();
+                btn.Text = ("Görev " + i).ToString();
+                btn.Name = "btn" + i;
+                btn.Tag = i+1;
+                btn.Click += new EventHandler(btnListi_Click);
+
+                dugmeler.Add(btn);
+                flowLayoutPanel1.Controls.Add(dugmeler[i]);
+            }
+            MessageBox.Show(dugmeler.Count + " yeni buton eklendi.");
+            btnRenklendir.Show();
+        }
+
+        private void btnListi_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.Name == "btn0")
+            {
+                MessageBox.Show("1. butona tıklandı");
+            }
+            else
+            {
+                
+                MessageBox.Show(String.Format(@"{0}. butona tıklandı" , btn.Tag));
+            }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            List<Color> cr = new List<Color>();
+            cr.Add(Color.Black);
+            cr.Add(Color.Red);
+            cr.Add(Color.Blue);
+            foreach (Color item2 in cr)
+            {
+                foreach (Button item in dugmeler) 
+                { 
+                    item.BackColor = item2;
+                } 
+                
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
